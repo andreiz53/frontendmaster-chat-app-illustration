0372b0d22dd049f358307f178231e3603c5f6f37
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-12 21:09:51 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div id="page-container">
+    <div class="main-section">
+      <div class="left-bg"></div>
+      <div class="right-bg"></div>
+      <div class="main-row">
+        <div class="left-col"></div>
+        <div class="right-col">
+          <h1>Simple booking</h1>
+          <p>
+            Stay in touch with our dog walkers through the chat interface. This
+            makes it easy to discuss arrangements and make bookings. Once the
+            walk has been completed you can rate your walker and book again all
+            through the chat.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
