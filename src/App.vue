6f6c0bf8cdeb007a34cb1fcb8d08fd036299a6aa
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-12 21:38:03 +0300

    complete html structure

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,66 @@
       <div class="left-bg"></div>
       <div class="right-bg"></div>
       <div class="main-row">
-        <div class="left-col"></div>
+        <div class="left-col">
+          <div class="phone">
+            <div class="speaker"></div>
+            <div class="chat-header">
+              <div class="back"></div>
+              <div class="person-info">
+                <div class="person-name"></div>
+                <div class="person-status"></div>
+              </div>
+              <div class="chat-settings">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+              </div>
+            </div>
+            <div class="chat-body">
+              <div class="chat-block">
+                <div class="sender-message">
+                  That sounds great. I’d be happy with that.
+                </div>
+                <div class="sender-message">
+                  Could you send over some pictures of your dog, please?
+                </div>
+              </div>
+              <div class="chat-block">
+                <div class="my-images">
+                  <img src="./assets/dog-image-1.jpg" alt="dog image" />
+                  <img src="./assets/dog-image-2.jpg" alt="dog image" />
+                  <img src="./assets/dog-image-3.jpg" alt="dog image" />
+                </div>
+                <div class="my-message">
+                  Here are a few pictures. She’s a happy girl!
+                </div>
+                <div class="my-message">Can you make it?</div>
+              </div>
+              <div class="chat-block">
+                <div class="sender-message">
+                  She looks so happy! The time we discussed works. How long
+                  shall I take her out for?
+                </div>
+                <div class="sender-service">
+                  <div class="service-check"></div>
+                  <div class="service-message">30 minute walk</div>
+                  <div class="service-price">$29</div>
+                </div>
+                <div class="sender-service">
+                  <div class="service-check"></div>
+                  <div class="service-message">1 hour walk</div>
+                  <div class="service-price">$49</div>
+                </div>
+              </div>
+            </div>
+            <div class="chat-input">
+              <form>
+                <input type="text" name="message" id="message" />
+                <button type="submit"></button>
+              </form>
+            </div>
+          </div>
+        </div>
         <div class="right-col">
           <h1>Simple booking</h1>
           <p>
